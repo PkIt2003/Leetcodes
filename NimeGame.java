5ffac099f77ff04445dfd33407a87c4ce64cca23
premkumar.b <112531986+PkIt2003@users.noreply.github.com>
2024-12-11 15:30:21 +0530

    Given n, the number of stones in the heap, return true if you can win the game assuming
    You are playing the following Nim Game with your friend:
    
    Initially, there is a heap of stones on the table.
    You and your friend will alternate taking turns, and you go first.
    On each turn, the person whose turn it is will remove 1 to 3 stones from the heap.
    The one who removes the last stone is the winner.
    Given n, the number of stones in the heap, return true if you can win the game assuming both you and your friend play optimally, otherwise return false.

---
+++ NimeGame.java
@@ -0,0 +1,11 @@
+public class NimeGame {
+        // id-292. Nim Game
+        public boolean canWinNim(int n) {
+            if(n%4==0){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+}
